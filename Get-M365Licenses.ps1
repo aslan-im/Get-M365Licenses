f29bc26badaa7c38588bdbba7fbc95aa87ea65fd
Aslan Imanalin <7303058+aslan-im@users.noreply.github.com>
2021-10-14 09:20:43 +0300

    Code cosmetic improvements

--- Get-M365Licenses.ps1
+++ Get-M365Licenses.ps1
@@ -22,68 +22,68 @@ catch {
 
 #List of SkuIDs and friendly names
 $LicensesDictionary = @{
-    'STREAM' = 'Microsoft Stream Trial';
-    'POWER_BI_PRO' = 'Power BI Pro';
-    'SPZA_IW' = 'App Connect';
-    'WINDOWS_STORE' = 'Windows Store for Business';
-    'FLOW_FREE' = 'Microsoft Flow Free';
-    'MICROSOFT_BUSINESS_CENTER'	= 'Microsoft Business Center';
-    'MCOEV' = 'Microsoft 365 Phone System';
-    'CCIBOTS_PRIVPREV_VIRAL' = 'Dynamics Bots Trial';
-    'FORMS_PRO' = 'Forms Pro Trial';
-    'POWERAPPS_VIRAL' = 'Microsoft PowerApps Plan 2 Trial';
-    'MCOCAP' = 'Common Area Phone';
-    'CRMTESTINSTANCE' = 'Microsoft Dynamics CRM Test Instance';
-    'DYN365_ENTERPRISE_PLAN1' = 'Dynamics 365 Customer Engagement Plan Enterprise Edition';
-    'MEETING_ROOM' = 'Microsoft Teams Rooms Standard';
-    'POWER_BI_STANDARD' = 'Power BI (free)';
-    'MCOPSTNC' = 'Communications Credits';
-    'ADALLOM_STANDALONE' = 'Microsoft Cloud App Security';
-    'TEAMS_EXPLORATORY' = 'Microsoft Teams Exploratory';
-    'MCOMEETADV' = 'Microsoft 365 Audio Conferencing';
-    'CRMINSTANCE' = 'Microsoft Dynamics CRM Instance';
-    'SPE_E3' = 'Microsoft 365 E3';
-    'MCOPSTN2' = 'Microsoft 365 Domestic and International Callin plan';
-    'CRMSTORAGE' = 'Microsoft Dynamics CRM Storage';
-    'RIGHTSMANAGEMENT_ADHOC' = 'Rights Management Adhoc';
-    'STANDARDPACK' = 'Office 365 E1';
-    'EMSPREMIUM'	=	'Enterprise Mobility + Security E5';
-    'FLOW_P1'	=	'Microsoft Flow Plan 1';
-    'MICROSOFT_REMOTE_ASSIST'	=	'Dynamics 365 Remote Assist';
-    'ENTERPRISEPREMIUM'	=	'Office 365 E5';
-    'FLOW_PER_USER'	=	'Power Automate per user plan';
-    'DYN365_AI_SERVICE_INSIGHTS'	=	'Trial Dynamics 365 Customer Service Insights';
-    'POWERFLOW_P1'	=	'Microsoft Power Automate Plan 1';
-    'ENTERPRISEPACK'	=	'Office 365 E3';
-    'M365_E5_SUITE_COMPONENTS'	=	'Microsoft 365 E5 Suite features';
-    'PROJECTESSENTIALS'	=	'Project Online Essentials';
-    'M365_F1'	=	'Microsoft 365 F1';
-    'DESKLESSPACK'	=	'Office 365 F1';
-    'OFFICE365_MULTIGEO'	=	'Multi-Geo Capabilities in Office 365';
-    'PROJECT_P1'	=	'Project Plan 1';
-    'PROJECTPREMIUM'	=	'Project Plan 5';
-    'PBI_PREMIUM_P1_ADDON'	=	'Power BI Premium Plan 1 Addon';
-    'EXCHANGESTANDARD'	=	'Exchange Online Plan 1';
-    'DYN365_ENTERPRISE_P1_IW'	=	'Dynamics 365 P1 Trial for Information Workers';
-    'DYN365_ENTERPRISE_CUSTOMER_SERVICE'	=	'Dynamics 365 for Customer Service Enterprise Edition';
-    'WIN_DEF_ATP'	=	'Microsoft Defender Advanced Threat Protection';
-    'POWERFLOW_P2'	=	'Microsoft Power Apps Plan 2';
-    'POWERAPPS_PER_USER'	=	'Power Apps Per User Plan';
-    'EMS'	=	'Enterprise Mobility + Security E3';
-    'PBI_PREMIUM_P2_ADDON'	=	'Power BI Premium Plan 2 Addon';
-    'M365_F1_COMM'	=	'Microsoft 365 F1';
-    'AAD_PREMIUM'	=	'Azure Active Directory Premium P1';
-    'PROJECTPROFESSIONAL'	=	'Project Online Professional';
-    'EXCHANGEENTERPRISE'	=	'Exchange Online Plan 2';
-    'SPE_F1'	=	'Microsoft 365 F3';
-    'WORKPLACE_ANALYTICS'	=	'Microsoft Workplace Analytics';
-    'POWERAPPS_PER_APP'	=	'Power Apps Per Application';
-    'DYN365_ENTERPRISE_TEAM_MEMBERS'	=	'Dynamics 365 for Team Members Enterprise Edition';
-    'POWERAPPS_PER_APP_IW' = 'PowerApps per app baseline access';
-    'PHONESYSTEM_VIRTUALUSER' = 'Microsoft 365 Phone System - Virtual Usuer';
-    'EXCHANGEARCHIVE_ADDON' = 'Exchange Online Archiving addon';
-    'RIGHTSMANAGEMENT' = 'Azure Information Protection Premium P1';
-    'CDSAICAPACITY' = 'AI Builder Capacity add-on';
+    'STREAM'                                = 'Microsoft Stream Trial';
+    'POWER_BI_PRO'                          = 'Power BI Pro';
+    'SPZA_IW'                               = 'App Connect';
+    'WINDOWS_STORE'                         = 'Windows Store for Business';
+    'FLOW_FREE'                             = 'Microsoft Flow Free';
+    'MICROSOFT_BUSINESS_CENTER'	            = 'Microsoft Business Center';
+    'MCOEV'                                 = 'Microsoft 365 Phone System';
+    'CCIBOTS_PRIVPREV_VIRAL'                = 'Dynamics Bots Trial';
+    'FORMS_PRO'                             = 'Forms Pro Trial';
+    'POWERAPPS_VIRAL'                       = 'Microsoft PowerApps Plan 2 Trial';
+    'MCOCAP'                                = 'Common Area Phone';
+    'CRMTESTINSTANCE'                       = 'Microsoft Dynamics CRM Test Instance';
+    'DYN365_ENTERPRISE_PLAN1'               = 'Dynamics 365 Customer Engagement Plan Enterprise Edition';
+    'MEETING_ROOM'                          = 'Microsoft Teams Rooms Standard';
+    'POWER_BI_STANDARD'                     = 'Power BI (free)';
+    'MCOPSTNC'                              = 'Communications Credits';
+    'ADALLOM_STANDALONE'                    = 'Microsoft Cloud App Security';
+    'TEAMS_EXPLORATORY'                     = 'Microsoft Teams Exploratory';
+    'MCOMEETADV'                            = 'Microsoft 365 Audio Conferencing';
+    'CRMINSTANCE'                           = 'Microsoft Dynamics CRM Instance';
+    'SPE_E3'                                = 'Microsoft 365 E3';
+    'MCOPSTN2'                              = 'Microsoft 365 Domestic and International Callin plan';
+    'CRMSTORAGE'                            = 'Microsoft Dynamics CRM Storage';
+    'RIGHTSMANAGEMENT_ADHOC'                = 'Rights Management Adhoc';
+    'STANDARDPACK'                          = 'Office 365 E1';
+    'EMSPREMIUM'                            = 'Enterprise Mobility + Security E5';
+    'FLOW_P1'                               = 'Microsoft Flow Plan 1';
+    'MICROSOFT_REMOTE_ASSIST'	            = 'Dynamics 365 Remote Assist';
+    'ENTERPRISEPREMIUM'	                    = 'Office 365 E5';
+    'FLOW_PER_USER'	                        = 'Power Automate per user plan';
+    'DYN365_AI_SERVICE_INSIGHTS'	        = 'Trial Dynamics 365 Customer Service Insights';
+    'POWERFLOW_P1'	                        = 'Microsoft Power Automate Plan 1';
+    'ENTERPRISEPACK'	                    = 'Office 365 E3';
+    'M365_E5_SUITE_COMPONENTS'	            = 'Microsoft 365 E5 Suite features';
+    'PROJECTESSENTIALS'	                    = 'Project Online Essentials';
+    'M365_F1'	                            = 'Microsoft 365 F1';
+    'DESKLESSPACK'	                        = 'Office 365 F1';
+    'OFFICE365_MULTIGEO'	                = 'Multi-Geo Capabilities in Office 365';
+    'PROJECT_P1'	                        = 'Project Plan 1';
+    'PROJECTPREMIUM'	                    = 'Project Plan 5';
+    'PBI_PREMIUM_P1_ADDON'	                = 'Power BI Premium Plan 1 Addon';
+    'EXCHANGESTANDARD'	                    = 'Exchange Online Plan 1';
+    'DYN365_ENTERPRISE_P1_IW'	            = 'Dynamics 365 P1 Trial for Information Workers';
+    'DYN365_ENTERPRISE_CUSTOMER_SERVICE'    = 'Dynamics 365 for Customer Service Enterprise Edition';
+    'WIN_DEF_ATP'	                        = 'Microsoft Defender Advanced Threat Protection';
+    'POWERFLOW_P2'	                        = 'Microsoft Power Apps Plan 2';
+    'POWERAPPS_PER_USER'	                = 'Power Apps Per User Plan';
+    'EMS'	                                = 'Enterprise Mobility + Security E3';
+    'PBI_PREMIUM_P2_ADDON'	                = 'Power BI Premium Plan 2 Addon';
+    'M365_F1_COMM'	                        = 'Microsoft 365 F1';
+    'AAD_PREMIUM'	                        = 'Azure Active Directory Premium P1';
+    'PROJECTPROFESSIONAL'	                = 'Project Online Professional';
+    'EXCHANGEENTERPRISE'	                = 'Exchange Online Plan 2';
+    'SPE_F1'	                            = 'Microsoft 365 F3';
+    'WORKPLACE_ANALYTICS'	                = 'Microsoft Workplace Analytics';
+    'POWERAPPS_PER_APP'	                    = 'Power Apps Per Application';
+    'DYN365_ENTERPRISE_TEAM_MEMBERS'	    = 'Dynamics 365 for Team Members Enterprise Edition';
+    'POWERAPPS_PER_APP_IW'                  = 'PowerApps per app baseline access';
+    'PHONESYSTEM_VIRTUALUSER'               = 'Microsoft 365 Phone System - Virtual Usuer';
+    'EXCHANGEARCHIVE_ADDON'                 = 'Exchange Online Archiving addon';
+    'RIGHTSMANAGEMENT'                      = 'Azure Information Protection Premium P1';
+    'CDSAICAPACITY'                         = 'AI Builder Capacity add-on';
 }
 
 #Getting first SKU for generating tenant name
@@ -93,13 +93,30 @@ $OrganizationName,$sku = $firstSKU -split ':'
 $ReportName = $CurrentDate + '_' + $OrganizationName + '.xlsx'
 
 #Formatting the result and exporting to .xlsx
-$LicensesToExport = $AllLicenses | Select-Object @{L = 'Product Title'; E = {
-    $(    
-        [string]$CompanyName, [string]$SkuID = $_.AccountSkuId -split ':'
-        $LicensesDictionary[$SkuID]
-    )
-}}, @{L = 'Valid Licenses'; E = {$($($_.ActiveUnits) - $($_.ConsumedUnits))}}, `
-@{L='Assigned License'; E={$($_.ConsumedUnits)}}, @{L = 'Licenses Total'; E = {$_.ActiveUnits}}
+$LicensesSelection = @(
+    @{
+        L = 'Product Title'; 
+        E = {
+            $([string]$CompanyName, [string]$SkuID = $_.AccountSkuId -split ':'
+            $LicensesDictionary[$SkuID])
+        }
+    }, 
+    @{
+        L = 'Valid Licenses'; 
+        E = {
+            $($($_.ActiveUnits) - $($_.ConsumedUnits))
+        }
+    },
+    @{
+        L='Assigned License'; 
+        E={$($_.ConsumedUnits)}},
+    @{
+        L = 'Licenses Total'; 
+        E = {$_.ActiveUnits}
+    }
+)
+
+$LicensesToExport = $AllLicenses | Select-Object $LicensesSelection
 
 try {
     $LicensesToExport | Export-Excel -Path $ReportName -AutoSize -AutoFilter -TableStyle Medium2
